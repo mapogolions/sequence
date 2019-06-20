2dcb5f271e4a9d6097e6d3041cd1151928d97a72
Pavel <ivanovpavelalex45@gmail.com>
2019-06-20 21:33:45 +0500

    Spellcheck

--- test/isEmpty.test.js
+++ test/isEmpty.test.js
@@ -5,7 +5,7 @@ const empty = require('../src/empty.js');
 const pure = require('../src/pure.js');
 const cons = require('../src/cons.js');
 
-test('check if the sequence is empty', () => {
+test('checks if the sequence is empty', () => {
   expect(isEmpty(empty)).toBe(true);
   expect(isEmpty(pure(1))).toBe(false);
   expect(isEmpty(cons(0, empty))).toBe(false);
