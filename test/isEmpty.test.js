f3d2914f086a6bd8650b13e904e73c12be5d088d
Pavel <ivanovpavelalex45@gmail.com>
2019-06-20 21:32:29 +0500

    Added tests of isEmpty function

---
+++ test/isEmpty.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const isEmpty = require('../src/isEmpty.js');
+const empty = require('../src/empty.js');
+const pure = require('../src/pure.js');
+const cons = require('../src/cons.js');
+
+test('check if the sequence is empty', () => {
+  expect(isEmpty(empty)).toBe(true);
+  expect(isEmpty(pure(1))).toBe(false);
+  expect(isEmpty(cons(0, empty))).toBe(false);
+});
