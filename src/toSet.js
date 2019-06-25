9c170749c653610eb85eb5b76833caec60cc9938
Pavel <ivanovpavelalex45@gmail.com>
2019-06-25 11:21:10 +0500

    Changed implementation of toSet function

--- src/toSet.js
+++ src/toSet.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const { Nil } = require('./adt.js');
+const fold = require('./fold.js');
 
-const toSet = thunk => {
-  const iter = (acc, current) => {
-    const item = current();
-    if (item === Nil) return acc;
-    const { head, tail } = item;
-    return iter(acc.add(head), tail);
-  };
-  return iter(new Set(), thunk);
-};
-
-module.exports = toSet;
+module.exports = thunk => fold((acc, x) => acc.add(x), new Set(), thunk);
