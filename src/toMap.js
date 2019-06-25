9ec673cd2c3aca0a05c69df5d9c09f49009f337a
Pavel <ivanovpavelalex45@gmail.com>
2019-06-25 11:29:56 +0500

    Changed implementation of toMap function

--- src/toMap.js
+++ src/toMap.js
@@ -1,15 +1,5 @@
 'use strict';
 
-const { Nil } = require('./adt.js');
+const foldi = require('./foldi.js');
 
-const toMap = thunk => {
-  const iter = (index, acc, current) => {
-    const item = current();
-    if (item === Nil) return acc;
-    const { head, tail } = item;
-    return iter(index + 1, acc.set(index, head), tail);
-  };
-  return iter(0, new Map(), thunk);
-};
-
-module.exports = toMap;
+module.exports = thunk => foldi((acc, x, i) => acc.set(i, x), new Map(), thunk);
